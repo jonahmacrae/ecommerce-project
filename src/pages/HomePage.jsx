0bf63af3a2cf90602900a87d775cdac6c8959bc6
jonahmacrae <jonahmacrae@gmail.com>
2025-10-21 09:17:43 +0100

    Separate formatCurrency into a utility function so that it can be used across the project

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Header } from "../components/Header.jsx";
+import { formatCurrency } from "../components/formatCurrency.jsx";
 import { products } from "../../starting-code/data/products.js";
 
 import "./HomePage.css";
@@ -15,10 +16,7 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -28,7 +26,9 @@ export function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                    src={`images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png`}
                   />
                   <div className="product-rating-count link-primary">
                     {product.rating.count}
@@ -36,7 +36,7 @@ export function HomePage() {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  ${formatCurrency(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
