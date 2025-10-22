917dbcbda0543bc87c61ded96d4c2731854751d1
jonahmacrae <jonahmacrae@gmail.com>
2025-10-22 11:29:26 +0100

    searching resets contents of search input

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,45 +1,59 @@
-import { Link } from 'react-router';
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import { Link } from "react-router";
 
-import './header.css';
+import "./header.css";
 
 export function Header({ cart }) {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
   let totalQuantity = 0;
-  
+
   cart.forEach((cartItem) => {
     totalQuantity += cartItem.quantity;
   });
 
+  const searchClick = () => {
+    console.log("Searching for:", search);
+    navigate(`/?search=${search}`);
+    setSearch("");
+  };
+
   return (
     <div className="header">
-        <div className="left-section">
-          <Link to="/" className="header-link">
-            <img className="logo"
-              src="images/logo-white.png" />
-            <img className="mobile-logo"
-              src="images/mobile-logo-white.png" />
-          </Link>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <Link className="orders-link header-link" to="/orders">
-
-            <span className="orders-text">Orders</span>
-          </Link>
-
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">{totalQuantity}</div>
-            <div className="cart-text">Cart</div>
-          </Link>
-        </div>
+      <div className="left-section">
+        <Link to="/" className="header-link">
+          <img className="logo" src="images/logo-white.png" />
+          <img className="mobile-logo" src="images/mobile-logo-white.png" />
+        </Link>
       </div>
+
+      <div className="middle-section">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <button className="search-button" onClick={searchClick}>
+          <img className="search-icon" src="images/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <Link className="orders-link header-link" to="/orders">
+          <span className="orders-text">Orders</span>
+        </Link>
+
+        <Link className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">{totalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </Link>
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,22 +1,31 @@
 import axios from "axios";
+import { useSearchParams,  } from "react-router";
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header.jsx";
 import { ProductsGrid } from "./ProductsGrid.jsx";
 import "./HomePage.css";
 
-const BACKEND_PRODUCTS_URL = "/api/products";
+const BACKEND_BASE_PATH = "http://localhost:5173/";
+const BACKEND_PRODUCTS_PATH = "/api/products";
 
 export function HomePage({ cart, loadCart }) {
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchHomeData = async () => {
-      const response = await axios.get(BACKEND_PRODUCTS_URL);
+      const url = new URL(BACKEND_PRODUCTS_PATH, BACKEND_BASE_PATH);
+      if (search) {
+        url.searchParams.set('search', search);
+      }
+      const response = await axios.get(url.pathname + url.search);
       setProducts(response.data);
     };
 
     fetchHomeData();
-  }, []);
+  }, [search]);
 
   return (
     <>
