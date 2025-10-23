925cb5d14a6d004ae72d8994d2c010e9c43e6c8e
jonahmacrae <jonahmacrae@gmail.com>
2025-10-23 09:05:52 +0100

    Create additional tests for formatCurrency

--- frontend/src/utils/money.test.js
+++ frontend/src/utils/money.test.js
@@ -10,4 +10,13 @@ describe("formatCurrency", () => {
     expect(formatCurrency(1090)).toBe("10.90");
     expect(formatCurrency(100)).toBe("1.00");
   });
+
+  it("works with 0", () => {
+    expect(formatCurrency(0)).toBe("0.00");
+  })
+
+  it("works with negative numbers", () => {
+    expect(formatCurrency(-100)).toBe("-1.00");
+    expect(formatCurrency(-999)).toBe("-9.99");
+  })
 });
